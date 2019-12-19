fb2fa69417a03ee382eb8c8076ecfa0d1de4ec26
Hartvigen <rhartv17@student.aau.dk>
2019-12-19 23:54:21 +0100

    now saves transparent images and discards others

--- chan_scrapy/main.py
+++ chan_scrapy/main.py
@@ -1,3 +1,23 @@
 import subprocess
+import os
+import shutil
+import time
+from PIL import Image
+from trans import has_transparency
 
-subprocess.Popen('scrapy crawl chanCrawler', shell=True)
+download_folder = "chan_scrapy\\DownloadedImages\\full"
+kept_folder = "chan_scrapy\\KeptImages"
+
+#downloader = subprocess.Popen('scrapy crawl chanCrawler', shell=True)
+#downloader.wait()
+
+for file in os.listdir(download_folder):
+    filename = os.fsdecode(file)
+    if filename.endswith(".png") or filename.endswith(".jpg"):
+        im = Image.open(download_folder + "\\" + file, 'r')
+        if has_transparency(im):
+            shutil.move(download_folder + "\\" + file, kept_folder + "\\" + file)
+            print(filename + " is transparent!")
+
+
+shutil.rmtree(download_folder)
---
+++ chan_scrapy/trans.py
@@ -0,0 +1,14 @@
+def has_transparency(img):
+    if img.mode == "P":
+        transparent = img.info.get("transparency", -1)
+        for _, index in img.getcolors():
+            if index == transparent:
+                print(index)
+                return True
+    elif img.mode == "RGBA":
+        extrema = img.getextrema()
+        if extrema[3][0] < 180:
+            print(extrema)
+            return True
+
+    return False
